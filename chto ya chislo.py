a97a481fd2b696768263be9dd6c2e03455dced1d
Alice <gnomikvassya+gh@gmail.com>
2018-04-21 11:35:23 +0300

    correct format

--- chto ya chislo.py
+++ chto ya chislo.py
@@ -1,4 +1,16 @@
-input('Загадайте число от 1 до %d' % (2 ** 30))
-a = int(input('Это число меньше 536870912 ? В ответ напишите цифру 0 (нет) или 1 (да)'))
-if a == 0:
-    a = int(input('Это число меньше  ?'))
+interval_size = 2 ** 10
+
+print('Загадайте число от 0 до %d' % (interval_size - 1))
+
+st = 0
+fin = interval_size
+
+while st != (fin-1):
+    mid = (fin - st) // 2 + st
+    a = input('Это число меньше {} ?'.format(mid))
+    if a.lower() in ['да', 'lf', 'zes', 'yes', 'ja', 'нуы', 'оф', 'oui', 'щгш']:
+        fin = mid
+    else:
+        st = mid
+
+print('Ваше число %d' % st)
